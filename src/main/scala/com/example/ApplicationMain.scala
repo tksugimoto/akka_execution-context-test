20c3dcd80fb644d70e193e00dfd66c5d6e2df37d
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-08-10 17:28:57 +0900

    [Add] thread-pool-executorのタスクキューの設定の挙動確認

--- src/main/scala/com/example/ApplicationMain.scala
+++ src/main/scala/com/example/ApplicationMain.scala
@@ -1,12 +1,52 @@
 package com.example
 
 import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.{Future, ExecutionContextExecutor}
 
 object ApplicationMain extends App {
-  val system = ActorSystem("MyActorSystem")
-  val pingActor = system.actorOf(PingActor.props, "pingActor")
-  pingActor ! PingActor.Initialize
-  // This example app will ping pong 3 times and thereafter terminate the ActorSystem - 
-  // see counter logic in PingActor
+  
+  val customConf = ConfigFactory.parseString("""
+      akka.actor {
+        default-dispatcher {
+          default-executor {
+            fallback = "thread-pool-executor"
+          }
+          thread-pool-executor {
+            # キューの上限なし
+            task-queue-size = -1
+            
+            
+            # Minimum number of threads to cap factor-based max number to
+            # (if using a bounded task queue)            
+            max-pool-size-min = 8
+            
+            # Max no of threads (if using a bounded task queue) is determined by
+            # calculating: ceil(available processors * factor)
+            max-pool-size-factor  = 3.0
+            
+            # Max number of threads to cap factor-based max number to
+            # (if using a  bounded task queue)
+            max-pool-size-max = 10
+          }
+        }
+      }
+  """)
+  
+  val system = ActorSystem("MyActorSystem", ConfigFactory.load(customConf))
+  
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+    
+  println("foreach: start")
+  (1 to 50).foreach { n =>
+    Future {
+      println(s"start: ${n}")
+      Thread.sleep(3000)
+      println(s"end: ${n}")
+    }
+  }
+  println("foreach: end")
+
   system.awaitTermination()
 }
